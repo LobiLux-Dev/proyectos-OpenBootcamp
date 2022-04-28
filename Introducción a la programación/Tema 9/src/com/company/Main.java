5f8f6d249f8994e47c64818c618fb581c121728d
Luis Sanchez <luis.sanchezz.vazquezz@gmail.com>
2022-04-28 01:18:56 -0500

    Tema 9

--- Introducción a la programación/Tema 9/src/com/company/Main.java
+++ Introducción a la programación/Tema 9/src/com/company/Main.java
@@ -35,18 +35,23 @@ class Persona {
     public int getEdad() {
         return edad;
     }
+    
     public void setEdad(int edad) {
         this.edad = edad;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public int getNumero() {
         return numero;
     }
+
     public void setNumero(int numero) {
         this.numero = numero;
     }
