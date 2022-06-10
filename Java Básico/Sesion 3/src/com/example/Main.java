ed04e9eb3a922b71c1fb61c0c23ca5d7f828de84
Luis Sanchez <luis.sanchezz.vazquezz@gmail.com>
2022-06-10 04:37:11 -0500

    Ejercicio 1 - Sesion 3

---
+++ Java Básico/Sesion 3/src/com/example/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String[] nombres = {"Jose", "Luis", "Alonso"};
+
+        for (String nombre : nombres){
+            System.out.print(nombre + " ");
+        }
+    }
+}
