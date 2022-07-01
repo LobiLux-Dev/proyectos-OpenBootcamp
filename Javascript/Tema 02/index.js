fe8880d630b180ab47e4e066a08908e2be691595
Luis Sanchez <luis.sanchezz.vazquezz@gmail.com>
2022-07-01 01:26:18 -0500

    Ejercicio 1 - Tema 02

---
+++ Javascript/Tema 02/index.js
@@ -0,0 +1,12 @@
+const lista = [
+  'Luis Sanchez',
+  16,
+  true,
+  new Date(2005, 8, 5),
+  {
+    titulo: 'La maldición del Hotel California',
+    autor: 'Juan Antonio Amezcua Castillo',
+    fecha: new Date(2013, 6, 1),
+    url: 'https://www.gonvill.com.mx/libro/maldicion-del-hotel-california-la-mas-lectores_37660103'
+  }
+]
