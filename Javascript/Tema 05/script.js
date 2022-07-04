31e6b86f247bb36868a2db4c2ed55b859542a461
Luis Sanchez <luis.sanchezz.vazquezz@gmail.com>
2022-07-04 07:47:18 -0500

    Ejercicio 1 - Tema 05

---
+++ Javascript/Tema 05/script.js
@@ -0,0 +1,12 @@
+// Una variable que contenga tu altura en centímetros (entero)
+let alturaCm = 167
+// Una variable que contenga tu altura en metros (número de coma flotante)
+let alturaMt = 1.67
+// Una variable que contenga tu peso en kilogramos (número de coma flotante)
+let pesoKg = 53.2
+// Una variable que contenga tu altura en metros redondeada hacia arriba
+let alturaMtRounded = Math.ceil(alturaMt)
+// Una variable que contenga tu peso en kilogramos redondeado hacia abajo
+let pesoKgRounded = Math.floor(pesoKg)
+// Una variable que contenga si "el máximo valor que se puede obtener en Javascript + 1" es igual al máximo valor que se puede obtener en Javascript
+let isMax = Number.MAX_VALUE + 1 === Number.MAX_VALUE
