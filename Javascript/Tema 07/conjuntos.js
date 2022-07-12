45f8f1d23e1a3e135a15865bdb56933174faeb16
Luis Sanchez <luis.sanchezz.vazquezz@gmail.com>
2022-07-12 16:50:05 -0500

    Ejercicio 1 - Tema 07

---
+++ Javascript/Tema 07/conjuntos.js
@@ -0,0 +1,6 @@
+// Un nuevo Set con los nombres de tu familia
+const nombres = new Set(['Luis', 'Belen'])
+// Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+nombres.add('Luis')
+// Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
+nombres.add('JavaScript')
