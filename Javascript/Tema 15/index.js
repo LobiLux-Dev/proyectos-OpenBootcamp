8e711e0ffcd37870a2d64f963c0f301ed7d6988c
Luis Sanchez <luis.sanchezz.vazquezz@gmail.com>
2022-08-11 10:08:46 -0500

    Ejercicio 1 - Tema 15

---
+++ Javascript/Tema 15/index.js
@@ -0,0 +1,10 @@
+let name = "Luis"
+let lastName = "Sanchez"
+let object = {
+    name,
+    lastName
+}
+
+// sessionStorage.setItem("object", JSON.stringify(object))
+// localStorage.setItem("object", JSON.stringify(object))
+// document.cookie = "object=" + JSON.stringify(object) + "; expires=" + new Date(Date.now() + (2 * 60 * 1000)).toUTCString()
